f69e0ca738be8592509a8ab57926b343295c92a3
Ashton Prigge <ashton.prigge@gmail.com>
2020-07-07 13:00:34 -0700

    SILSLA-13: Initial MARC manipulations.

--- vanguard.py
+++ vanguard.py
@@ -1,24 +1,8 @@
-from pymarc import Record, Field, MARCReader
+import sys
+from pymarc import Record, Field, MARCReader, MARCWriter
 
-import re
-
-file_name = '/Users/ashtonprigge/repos/pymarc_work/bib10000.mrc'
-
-"""
 #SILSLA-13
-def change_5xx_CLU(record):
-	for field in range(500, 600):
-		if field != 590:
-			if record[field] != None:
-				value = record[field].value()
-				print(value)
-				if record[field]['5'] != None:
-					subfield = record[field]['5'].value()
-					if bool(re.search(r'^CLU', str(subfield))) == True:
-						record['590'] = str(value)
-						record.remove_field(field)
-	return record
-		
+
 def change_CLU(record):
 	field_mapping = {
 					'655':'695',
@@ -29,35 +13,30 @@ def change_CLU(record):
 				}
 	for old_field in field_mapping.keys():
 		new_field = field_mapping[old_field]
-		if record[old_field] != None:
-			value = record[old_field].value()
-			subfield = record[old_field].get_subfields()
-			if record[old_field]['5'] != None:
-				subfield = record[old_field]['5'].value() 
-				if bool(re.search(r'^CLU', str(subfield))) == True:
-					field = Field(tag=new_field)
-
-					#record[new_field] = str(value)
+		for fld in record.get_fields(old_field):
+			for sfld in fld.get_subfields('5'):
+				if sfld.startswith('CLU'):
+					value = record[old_field]
+					field = Field(tag = new_field, data=value)
 					record.add_field(field)
-					record.remove_field(old_field)
+					record.remove_field(fld)
+					
 	return record
 
-def delete_752_CLU(record):
-	if record['752'] != None:
-		if record[old_field]['5'] != None:
-			subfield = record[old_field]['5'].value()
-			if bool(re.search(r'^CLU', str(subfield))) == True:
-				record.remove_field('752')
+def delete_752(record):
+	for fld in record.get_fields('752'):
+		for sfld in fld.get_subfields('5'):
+			if sfld.startswith('CLU'):
+				record.remove_field(fld)			
 	return record
-"""	
+
 def clean_record(file_name):
-	reader = MARCReader(open(file_name, 'rb'), permissive=True)
+	reader = MARCReader(open(file_name, 'rb'))
+	writer = MARCWriter(open('new_file.mrc', 'wb'))
 	for record in reader:
 		#change_5xx_CLU(record)
-		#change_CLU(record)
-		#delete_752_CLU(record)
-		subfield = record.get_subfields()
-		print(subfield)
-
-
-clean_record(file_name)
+		change_CLU(record)
+		delete_752(record)
+	writer.write(record)
+	writer.close()
+	reader.close()
