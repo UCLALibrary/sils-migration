6a7d381897535888de39142f090d2942857e540e
Ashton Prigge <ashton.prigge@gmail.com>
2020-07-08 09:06:37 -0700

    SILSLA_13: Refactored get_5xx_fields, change_CLU, and reader and writer.

--- vanguard.py
+++ vanguard.py
@@ -3,17 +3,14 @@ from pymarc import Record, Field, MARCReader, MARCWriter
 
 #SILSLA-13
 
-def modify_5xx(record):
+def get_5xx_fields(field_mapping_dict):
 	for old_field in range(500, 600):
 		if old_field != 590:
-			for fld in record.get_fields(old_field):
-				for sfld in fld.get_subfields('5'):
-					if sfld.startswith('CLU'):
-						fld.tag = '590'
-						record.add_ordered_field(fld)
-	return record
+			field_mapping_dict[old_field] = '590'
+
 
 def change_CLU(record):
+	"""Change field when original field's $5 starts with CLU"""
 	field_mapping = {
 					'655':'695',
 					'700':'970',
@@ -21,29 +18,31 @@ def change_CLU(record):
 					'730':'973',
 					'740':'974'
 				}
+	get_5xx_fields(field_mapping)
 	for old_field in field_mapping.keys():
 		new_field = field_mapping[old_field]
 		for fld in record.get_fields(old_field):
-			for sfld in fld.get_subfields('5'):
-				if sfld.startswith('CLU'):
-					fld.tag = new_field
-					record.add_ordered_field(fld)
-	return record
+			if fld['5'] != None and fld['5'].startswith('CLU'):
+				fld.tag = new_field
+				record.add_ordered_field(fld)
 
 def delete_752(record):
+	"""Delete 752 field if it's $5 starts with CLU"""
 	for fld in record.get_fields('752'):
-		for sfld in fld.get_subfields('5'):
-			if sfld.startswith('CLU'):
-				record.remove_field(fld)			
-	return record
+		if fld['5'] != None and fld['5'].startswith('CLU'):
+			record.remove_field(fld)			
 
-def clean_record(file_name=None, out_file=None):
-	reader = MARCReader(open(file_name, 'rb'))
-	writer = MARCWriter(open(outfile, 'wb'))
-	for record in reader:
-		modify_5xx(record)
-		change_CLU(record)
-		delete_752(record)
-		writer.write(record)
-	writer.close()
-	reader.close()
+if len(sys.argv) != 3:
+    raise ValueError(f'Usage: {sys.argv[0]} in_file out_file')
+
+reader = MARCReader(open(sys.argv[1], 'rb'))
+writer = MARCWriter(open(sys.argv[2], 'wb'))
+
+for record in reader:	
+	change_CLU(record)
+	delete_752(record)
+	writer.write(record)
+	print(record)
+
+writer.close()
+reader.close()
