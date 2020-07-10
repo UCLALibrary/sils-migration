d7efdcd43d9cb0fb9873fa346be4a6d0c74d7ed0
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2020-07-10 09:44:18 -0700

    SILSLA-17: Modify Suppressed Bib records (#10)
    * SILSLA-17: Modify OCLC 035 in suppressed bib records.
    * SILSLA-17: Fixed order of writer.write(record) in process_vanguard_bibs_script.
    * SILSLA-17: removed unneeded import.

---
+++ process_vanguard_suppressed_bibs.py
@@ -0,0 +1,27 @@
+import sys
+import copy
+from pymarc import Record, Field, MARCReader, MARCWriter
+
+#SILSLA-17
+
+def modify_suppressed_bibs(record):
+    """Replace (OCoLC) with (Suppressed) in 035 $a for suppressed records"""
+    for fld in record.get_fields("035"):
+        if fld["a"] is not None and fld["a"].startswith("(OCoLC)"):
+            fld["a"] = (fld["a"]).replace("(OCoLC)", "(Suppressed)")
+
+def do_SILSLA_17(record):
+    modify_suppressed_bibs(record)
+
+if len(sys.argv) != 3:
+    raise ValueError(f"Usage: {sys.argv[0]} in_file out_file")
+
+reader = MARCReader(open(sys.argv[1], "rb"))
+writer = MARCWriter(open(sys.argv[2], "wb"))
+
+for record in reader:
+    do_SILSLA_17(record)
+    writer.write(record)
+
+writer.close()
+reader.close()
