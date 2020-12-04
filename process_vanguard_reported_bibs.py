815d6ed1a1ac7a99b0f10eab5a51ed4d2a6c6b2c
Andy Kohler <akohler@library.ucla.edu>
2020-12-04 14:19:05 -0800

    SILSLA-47: Apply Suppressed to all OCoLC 035 subfields; handle bibs reported for deletion

--- process_vanguard_reported_bibs.py
+++ process_vanguard_reported_bibs.py
@@ -2,10 +2,10 @@ import sys
 import copy
 from pymarc import Record, Field, MARCReader, MARCWriter
 
-#SILSLA-17
+#SILSLA-47
 
-def modify_suppressed_bibs(record):
-    """Replace (OCoLC) with (Suppressed) in any 035 subfield for suppressed records"""
+def modify_reported_bibs(record):
+    """Replace (OCoLC) with (Reported) in any 035 subfield for reported records"""
     for fld in record.get_fields("035"):
         # 035 should not have multiple $a... but some records do
         # Subfield iteration and manipulation in pymarc is ... not good.
@@ -16,10 +16,10 @@ def modify_suppressed_bibs(record):
         for i in range(0, len(fld.subfields), 2):
             code, val = fld.subfields[i], fld.subfields[i+1]
             if val.startswith('(OCoLC)'):
-                fld.subfields[i+1] = val.replace('(OCoLC)', '(Suppressed)')
+                fld.subfields[i+1] = val.replace('(OCoLC)', '(Reported)')
 
-def do_SILSLA_17(record):
-    modify_suppressed_bibs(record)
+def do_SILSLA_47(record):
+    modify_reported_bibs(record)
 
 if len(sys.argv) != 3:
     raise ValueError(f"Usage: {sys.argv[0]} in_file out_file")
@@ -28,7 +28,7 @@ reader = MARCReader(open(sys.argv[1], "rb"))
 writer = MARCWriter(open(sys.argv[2], "wb"))
 
 for record in reader:
-    do_SILSLA_17(record)
+    do_SILSLA_47(record)
     writer.write(record)
 
 writer.close()
