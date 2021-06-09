bb6a543104dea51e82dde3c7af612575d88e6a7d
Andy Kohler <akohler@library.ucla.edu>
2021-06-08 20:05:33 -0800

    SILSLA-91: Delete 035 from MARCIVE case 2 bibs if 008/23 = 'o'

--- process_marcive_bibs.py
+++ process_marcive_bibs.py
@@ -9,6 +9,22 @@ Parameters:
 import sys
 from pymarc import MARCReader, MARCWriter
 
+def modify_035(record):
+	""" Change 035 prefix from (OCoLC) to (MARCIVE) """
+	for fld in record.get_fields('035'):
+		# Prefix should only be in $a, for these records
+		# 035 should not have multiple $a... but some records do
+		# Subfield iteration and manipulation in pymarc is ... not good.
+		# fld.get_subfields('a') returns only values
+		# fld.subfields returns a list
+		# with code,value,code,value etc. - not a list of tuples....
+		# Old-school loop over fld.subfields, getting 2 values (code,val) at a time;
+		# change relevant subfields.
+		for i in range(0, len(fld.subfields), 2):
+			code, val = fld.subfields[i], fld.subfields[i+1]
+			if val.startswith('(OCoLC)'):
+				fld.subfields[i+1] = val.replace('(OCoLC)', '(MARCIVE)')
+
 def delete_856(record):
 	""" Delete 856 field regardless of content """
 	for fld in record.get_fields('856'):
@@ -16,16 +32,22 @@ def delete_856(record):
 
 ### Main code starts here ###
 if len(sys.argv) != 3:
-    raise ValueError(f'Usage: {sys.argv[0]} in_file out_file')
+	raise ValueError(f'Usage: {sys.argv[0]} in_file out_file')
 reader = MARCReader(open(sys.argv[1], 'rb'))
 writer = MARCWriter(open(sys.argv[2], 'wb'))
 
 for record in reader:
+	# Update 035, only if 008/23 = o
+	# Don't make me iterate through non-repeatable fields...
+	format = record.get_fields('008')[0].value()[23:24]
+	if format == 'o':
+		modify_035(record)
+	
 	delete_856(record)
 
 	# Done making changes, save the changed record to file
 	writer.write(record)
-    	
+		
 # Cleanup
 writer.close()
 reader.close()
